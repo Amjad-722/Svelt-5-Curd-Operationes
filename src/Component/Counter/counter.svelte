1181ebcf48b2f90dedd27a97194e3037cf8f72b3
Amjad-722 <aliamjad722ee@gmail.com>
2024-11-05 17:22:49 +0500

    created counter

--- src/Component/Counter/counter.svelte
+++ src/Component/Counter/counter.svelte
@@ -4,7 +4,7 @@
     let double =$derived (count * 3);
     let minus =$derived (double - 2);
 
-
+//props
     let {number,name} = $props ()
 
     
@@ -28,9 +28,9 @@
 {count}
     </button>
     <p> {count} * 3 :{ double  === 3 ? "its even" : "its odd"} </p>
-    <p>The number is {number}</p>
     <p> {double} - 2 :{minus}</p>
+    <p class="mt-20 font-extrabolds text-2xl">The number is {number}</p>
+    <h1 class="text-2xl font-semibold pt-8">
+        this is my {name}
+    </h1>
 </div>
-<h1>
-    this is my {name}
-</h1>
