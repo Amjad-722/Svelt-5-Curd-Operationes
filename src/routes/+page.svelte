b105496741b1da65c0ce193b3966036a5eb8dc4e
Amjad-722 <aliamjad722ee@gmail.com>
2024-11-06 18:59:20 +0500

    added new logic based componnet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,114 +1,28 @@
 <script>
   import Counter from "../Component/Counter/counter.svelte";
+  import CrudOperationes from "../Component/Counter/crudOperationes/crudOperationes.svelte";
   import IfLogic from "../Component/Counter/ifLogic.svelte";
+  import Inputs from "../Component/Counter/Inputs.svelte";
 
-  let people = $state([]);
+ 
 
-  let prefix = $state("");
-  let first = $state("");
-  let last = $state("");
-  let i = $state(0);
-
-  let filteredPeople = $derived(
-    prefix
-      ? people.filter((person) => {
-          const name = `${person.last}, ${person.first}`;
-          return name.toLowerCase().startsWith(prefix.toLowerCase());
-        })
-      : people
-  );
-  let selected = $derived(filteredPeople[i]);
-
-  $effect(() => {
-    reset_inputs(selected);
-  });
-
-  function create() {
-    people = people.concat({ first, last });
-    i = people.length - 1;
-
-    first = "";
-    last = "";
-  }
-
-  function update() {
-    selected.first = first;
-    selected.last = last;
-    people = people;
-  }
-
-  function remove() {
-    const index = people.indexOf(selected);
-    people = [...people.slice(0, index), ...people.slice(index + 1)];
-
-    first = last = "";
-    i = Math.min(i, filteredPeople.length - 2);
-  }
-
-  function reset_inputs(person) {
-    first = person ? person.first : "";
-    last = person ? person.last : "";
-  }
+ 
   const Detail = {
     name: "amjad",
     number: 22,
   };
 </script>
+<div class="gap-4">
 
-<div><Counter {...Detail} /></div>
-<div><IfLogic /></div>
-
-<div class="h-screen flex flex-col justify-center items-center gap-4">
-  <label
-    ><input
-      bind:value={first}
-      placeholder="first"
-      class="border-2 border-gray-900 rounded-lg py-2 px-2"
-    /></label
-  >
-  <label
-    ><input
-      bind:value={last}
-      placeholder="last"
-      class="border-2 border-gray-900 rounded-lg py-2 px-2"
-    /></label
-  >
-
-  <select
-    bind:value={i}
-    size={5}
-    class="border-2 border-gray-900 rounded-lg w-1/3"
-  >
-    {#each filteredPeople as person, i}
-      <option value={i}>{person.last}, {person.first}</option>
-    {/each}
-  </select>
-
-  <div class="flex flex-row gap-20">
-    <input
-      placeholder="filter prefix"
-      bind:value={prefix}
-      class="border-2 border-gray-900 rounded-lg py-2"
-    />
-
-    <div class="">
-      <button
-        onclick={create}
-        disabled={!first || !last}
-        class="border-2 border-gray-900 rounded-lg p-1.5">create</button
-      >
-      <button
-        onclick={update}
-        disabled={!first || !last || !selected}
-        class="border-2 border-gray-900 rounded-lg p-1.5">update</button
-      >
-      <button
-        onclick={remove}
-        disabled={!selected}
-        class="border-2 border-gray-900 rounded-lg p-1.5">delete</button
-      >
-    </div>
-  </div>
+	<CrudOperationes/>
+	<hr class="border-1 border-gray-800">
+	<Counter {...Detail} />
+	<hr class="border-1 border-gray-800">
+	<IfLogic />
+	<hr class="border-1 border-gray-800">
+	<Inputs/>
 </div>
 
-<div class="sss"><Counter /></div>
+
+
+
