6d77492f68004d6d37a3980711f4715f63831945
Amjad-722 <aliamjad722ee@gmail.com>
2024-11-06 18:07:34 +0500

    added new logic based componnet

--- src/Component/Counter/ifLogic.svelte
+++ src/Component/Counter/ifLogic.svelte
@@ -1,28 +1,30 @@
 <script>
-  let user = $state({ loginIn:false });
+  let user = $state({ loginIn: false });
 
   function Toggle() {
     user.loginIn = !user.loginIn;
   }
-$effect(() => {
-    if(!user.loginIn){
-        alert("you are log out");
+  $effect(() => {
+    if (user.loginIn) {
+      alert("you are log out");
     }
-    
-})
- 
+  });
 </script>
-<div class="flex justify-center mt-20">
+<div class="flex flex-col justify-center items-center mt-10">
 
-
-{#if user.loginIn}
-<div>
-  <button class="border-2  p-2 rounded-lg shadow-lg shadow-gray-700  text-blue-600" onclick={Toggle}> Amjad you are login  </button>
-</div>
-{/if}
-{#if !user.loginIn}
-<div>
-<button class="border-2  p-2 rounded-lg shadow-lg shadow-gray-700  text-red-600" onclick={Toggle}> Opps! Amjad you are log out </button>
-</div>
-{/if}
+    {#if user.loginIn}
+      <button
+        class="border-2 p-2 rounded-lg shadow-lg shadow-gray-700 text-blue-600"
+        onclick={Toggle}
+      >
+        Amjad you are login
+      </button>
+    {:else}
+      <button
+        class="border-2 p-2 rounded-lg shadow-lg shadow-gray-700 text-red-600"
+        onclick={Toggle}
+      >
+        Opps! Amjad you are log out
+      </button>
+    {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,101 +1,114 @@
-
-
 <script>
   import Counter from "../Component/Counter/counter.svelte";
   import IfLogic from "../Component/Counter/ifLogic.svelte";
 
-	let people = $state([
-		
-	]);
+  let people = $state([]);
 
-	let prefix = $state('');
-	let first = $state('');
-	let last = $state('');
-	let i = $state(0);
+  let prefix = $state("");
+  let first = $state("");
+  let last = $state("");
+  let i = $state(0);
 
-   
+  let filteredPeople = $derived(
+    prefix
+      ? people.filter((person) => {
+          const name = `${person.last}, ${person.first}`;
+          return name.toLowerCase().startsWith(prefix.toLowerCase());
+        })
+      : people
+  );
+  let selected = $derived(filteredPeople[i]);
 
-	let filteredPeople = $derived(prefix
-		? people.filter((person) => {
-				const name = `${person.last}, ${person.first}`;
-				return name.toLowerCase().startsWith(prefix.toLowerCase());
-			})
-		: people);
-	let selected = $derived(filteredPeople[i]);
+  $effect(() => {
+    reset_inputs(selected);
+  });
 
-	$effect(() => {
-		reset_inputs(selected);
-	});
-
-	function create() {
+  function create() {
     people = people.concat({ first, last });
     i = people.length - 1;
-    
-   
-    first = '';
-    last = '';
-}
 
+    first = "";
+    last = "";
+  }
 
+  function update() {
+    selected.first = first;
+    selected.last = last;
+    people = people;
+  }
 
-	function update() {
-		selected.first = first;
-		selected.last = last;
-		people = people;
-	}
+  function remove() {
+    const index = people.indexOf(selected);
+    people = [...people.slice(0, index), ...people.slice(index + 1)];
 
-	function remove() {
-		
-		const index = people.indexOf(selected);
-		people = [...people.slice(0, index), ...people.slice(index + 1)];
+    first = last = "";
+    i = Math.min(i, filteredPeople.length - 2);
+  }
 
-		first = last = '';
-		i = Math.min(i, filteredPeople.length - 2);
-	}
-
-	function reset_inputs(person) {
-		first = person ? person.first : '';
-		last = person ? person.last : '';
-
-
-	}
-	const  Detail = {
-
-		name:"amjad",
-		number:22
-
-
-
-	};
-	
+  function reset_inputs(person) {
+    first = person ? person.first : "";
+    last = person ? person.last : "";
+  }
+  const Detail = {
+    name: "amjad",
+    number: 22,
+  };
 </script>
 
-<div><Counter {...Detail}/></div>
-<div><IfLogic/></div>
+<div><Counter {...Detail} /></div>
+<div><IfLogic /></div>
 
-<div class="h-screen  flex flex-col justify-center items-center gap-4">
-    <label><input bind:value={first} placeholder="first" class="border-2 border-gray-900 rounded-lg py-2 px-2" /></label>
-    <label><input bind:value={last} placeholder="last" class="border-2 border-gray-900 rounded-lg py-2 px-2" /></label>
-   
+<div class="h-screen flex flex-col justify-center items-center gap-4">
+  <label
+    ><input
+      bind:value={first}
+      placeholder="first"
+      class="border-2 border-gray-900 rounded-lg py-2 px-2"
+    /></label
+  >
+  <label
+    ><input
+      bind:value={last}
+      placeholder="last"
+      class="border-2 border-gray-900 rounded-lg py-2 px-2"
+    /></label
+  >
 
-    <select bind:value={i} size={5} class="border-2 border-gray-900 rounded-lg w-1/3">
-        {#each filteredPeople as person, i}
-            <option value={i}>{person.last}, {person.first}</option>
-        {/each}
-    </select>
-    
-  
-    <div class="flex flex-row gap-20">
-        <input placeholder="filter prefix" bind:value={prefix}  class="border-2 border-gray-900 rounded-lg py-2"/>
+  <select
+    bind:value={i}
+    size={5}
+    class="border-2 border-gray-900 rounded-lg w-1/3"
+  >
+    {#each filteredPeople as person, i}
+      <option value={i}>{person.last}, {person.first}</option>
+    {/each}
+  </select>
 
-        <div class="">
-            <button onclick={create} disabled={!first || !last} class="border-2 border-gray-900 rounded-lg p-1.5">create</button>
-            <button onclick={update} disabled={!first || !last || !selected} class="border-2 border-gray-900 rounded-lg p-1.5">update</button>
-        <button onclick={remove} disabled={!selected} class="border-2 border-gray-900 rounded-lg p-1.5">delete</button>
+  <div class="flex flex-row gap-20">
+    <input
+      placeholder="filter prefix"
+      bind:value={prefix}
+      class="border-2 border-gray-900 rounded-lg py-2"
+    />
+
+    <div class="">
+      <button
+        onclick={create}
+        disabled={!first || !last}
+        class="border-2 border-gray-900 rounded-lg p-1.5">create</button
+      >
+      <button
+        onclick={update}
+        disabled={!first || !last || !selected}
+        class="border-2 border-gray-900 rounded-lg p-1.5">update</button
+      >
+      <button
+        onclick={remove}
+        disabled={!selected}
+        class="border-2 border-gray-900 rounded-lg p-1.5">delete</button
+      >
     </div>
-    
-</div>
+  </div>
 </div>
 
-<div class="sss"><Counter/></div>
-
+<div class="sss"><Counter /></div>
