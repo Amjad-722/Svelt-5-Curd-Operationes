f1e5eaa2dbfa64b4afbb365cec27926c025eccd7
Amjad-722 <aliamjad722ee@gmail.com>
2024-11-06 17:18:23 +0500

    added new logic based componnet

--- src/Component/Counter/ifLogic.svelte
+++ src/Component/Counter/ifLogic.svelte
@@ -4,18 +4,25 @@
   function Toggle() {
     user.loginIn = !user.loginIn;
   }
+$effect(() => {
+    if(!user.loginIn){
+        alert("you are log out");
+    }
+    
+})
+ 
 </script>
 <div class="flex justify-center mt-20">
 
 
 {#if user.loginIn}
 <div>
-  <button class="border-2 border-neutral-900 p-2 rounded-lg shadow-lg shadow-gray-700  text-blue-600" onclick={Toggle}> Amjad you are login in </button>
+  <button class="border-2  p-2 rounded-lg shadow-lg shadow-gray-700  text-blue-600" onclick={Toggle}> Amjad you are login  </button>
 </div>
 {/if}
 {#if !user.loginIn}
 <div>
-<button onclick={Toggle}> Amjad ayour are log out </button>
+<button class="border-2  p-2 rounded-lg shadow-lg shadow-gray-700  text-red-600" onclick={Toggle}> Opps! Amjad you are log out </button>
 </div>
 {/if}
 </div>
